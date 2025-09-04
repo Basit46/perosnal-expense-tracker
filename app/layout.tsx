dccfa1d85fc86dd21a4c39f28bf193c51f16aa72
basit46 <hassanbasitope@gmail.com>
2025-09-04 10:35:55 +0100

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${poppins.variable} font-sans`}>
-        <RootLayoutContent children={children} />
+        <RootLayoutContent>{children}</RootLayoutContent>
       </body>
     </html>
   );
